963753c5aa4be3ddea26532ac0ba50a409891bf0
Francis <starsaintf@users.noreply.github.com>
2026-07-19 16:38:00 +0100

    perf: publish compressed Idea Zoo WebGL assets

--- scripts/validate_webgl_publish_contract.py
+++ scripts/validate_webgl_publish_contract.py
@@ -6,14 +6,14 @@ import sys
 
 REQUIRED_FILES = (
     "Build/IdeaZooWebGL.loader.js",
-    "Build/IdeaZooWebGL.data",
-    "Build/IdeaZooWebGL.framework.js",
-    "Build/IdeaZooWebGL.wasm",
+    "Build/IdeaZooWebGL.data.gz",
+    "Build/IdeaZooWebGL.framework.js.gz",
+    "Build/IdeaZooWebGL.wasm.gz",
 )
 REQUIRED_CONFIG = (
-    'dataUrl: buildUrl + "/IdeaZooWebGL.data"',
-    'frameworkUrl: buildUrl + "/IdeaZooWebGL.framework.js"',
-    'codeUrl: buildUrl + "/IdeaZooWebGL.wasm"',
+    'dataUrl: buildUrl + "/IdeaZooWebGL.data.gz"',
+    'frameworkUrl: buildUrl + "/IdeaZooWebGL.framework.js.gz"',
+    'codeUrl: buildUrl + "/IdeaZooWebGL.wasm.gz"',
 )
 
 
